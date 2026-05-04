ea132f41551e11a535eed25d52d2499b48e75cf9
Eliott059 <85693057+Eliott059@users.noreply.github.com>
2026-05-04 11:03:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from random import *
+from tkinter import *
+from playsound import *
 
+#valeur initial dès
+class valeur_dès_initiale:
+    def __init__(self):
+        self.d1 = randint(1, 6)
+        self.d2 = randint(1, 6)
+        self.d3 = randint(1, 6)
+        self.d4 = randint(1, 6)
+        self.d5 = randint(1, 6)
+
+#créer une liste des valeurs des dès
+    def liste_valeurs_dès(self):
+        return [self.d1, self.d2, self.d3, self.d4, self.d5]
+
+#fonction pour relancer les dès
+    def relancer_dès(self, dés_à_relancer):
