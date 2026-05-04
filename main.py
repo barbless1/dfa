90e363b5c11e943325a73668fa230ddb54bafb65
Eliott059 <85693057+Eliott059@users.noreply.github.com>
2026-05-04 11:37:12 +0200

    add tkinter class

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
 from random import *
 from tkinter import *
+from tkinter import ttk
 from playsound import *
 
-'''PARTIE 1 : interface graphique'''
 
+'''PARTIE 1 : interface graphique'''
+class InterfaceGraphique:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Fish dice adventure")
+        self.master.geometry("720x1080")
+
+        # Créer les éléments de l'interface
+        self.label = Label(self.master, text="Bienvenue au jeu de dés !")
+        self.label.pack(pady=10)
+
+        self.button_lancer = Button(self.master, text="Lancer les dés", command=self.lancer_des)
+        self.button_lancer.pack(pady=5)
+
+        self.button_relancer = Button(self.master, text="Relancer les dés", command=self.relancer_des)
+        self.button_relancer.pack(pady=5)
+
+        self.resultat_label = Label(self.master, text="")
+        self.resultat_label.pack(pady=10)
+
+    def lancer_des(self):
+        # Logique pour lancer les dés et afficher le résultat
+        pass
+
+    def relancer_des(self):
+        # Logique pour relancer les dés sélectionnés et afficher le résultat
+        pass
 
 
 
