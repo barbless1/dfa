3e545d6605162e0773915b07333b7fabbd7cae4d
barbless1 <rataumaile@gmail.com>
2026-05-07 22:49:22 +0200

    ajout de la mise à jour de la taille de la fenêtre et changement de la police des boutons

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from tkinter import * #Moteur graphique / Interface from PIL import Image, Image
 class ApplicationPrincipale:
     def __init__(self, racine):
         self.racine = racine
-        self.racine.geometry("1920x1080")
+        self.racine.geometry("1895x960") #Taille de la fenêtre accordée avec la taille de interface_partie_FDA.png 
         icon = PhotoImage(file="illustration/icone.png") #.ico est une extension propre à windows, pour que ça marche sur tous les systèmes, on utilise .png 
         self.racine.iconphoto(True, icon)
+
         # Créer un conteneur principal
         self.conteneur = Frame(self.racine)
         self.conteneur.pack(side="top", fill="both", expand=True)
@@ -42,19 +43,19 @@ class PageAccueil(Frame):
     
         
         # Bouton pour démarrer le jeu
-        bouton_jouer = Button(self, text="lancer !", font=("Helvetica", 14),
+        bouton_jouer = Button(self, text="lancer !", font=("Arial", 14),
                              command=lambda: controleur.afficher_page(InterfaceGraphique),
                              width=20, height=2)
         bouton_jouer.pack(pady=20)
         bouton_jouer.place(x=1471, y=665)
 
         # bouton mon invetaire (aquarium)
-        bouton_aquarium = Button(self, text="Mon aquarium", font=("Helvetica", 14), width=20, height=2, command=lambda: controleur.afficher_page(Aquarium))
+        bouton_aquarium = Button(self, text="Mon aquarium", font=("Arial", 14), width=20, height=2, command=lambda: controleur.afficher_page(Aquarium))
         bouton_aquarium.pack(pady=20)
         bouton_aquarium.place(x=50, y=780)
 
         #bouton shop
-        bouton_shop = Button(self, text="Shop", font=("Helvetica", 14), width=20, height=2, command=lambda: controleur.afficher_page(Shop))
+        bouton_shop = Button(self, text="Shop", font=("Arial", 14), width=20, height=2, command=lambda: controleur.afficher_page(Shop))
         bouton_shop.pack(pady=20)
         bouton_shop.place(x=50, y=850)
 
@@ -70,7 +71,7 @@ class InterfaceGraphique(Frame):
         self.canvas.create_image(0, 0, image=arriere_plan, anchor='nw')
         self.arriere_plan = arriere_plan
         '''
-        self.bouton_lancer = Button(self, text="Lancer les dés", font=("Helvetica", 18))
+        self.bouton_lancer = Button(self, text="Lancer les dés", font=("Arial", 18))
         self.bouton_lancer.pack(pady=5)
         
         #pour appeler la methode d'une autre classe, on doit d'abord créer une instance de cette classe, ici "jeu" est une instance de la classe "fonctions_du_jeu"
@@ -78,18 +79,18 @@ class InterfaceGraphique(Frame):
         self.bouton_lancer.config(command=self.jeu.lancer_des)
         self.bouton_lancer.place(x=1920/2-100, y=900)'''
         score = 0 
-        self.etiquette_resultat = Label(self, text=f"score : {score}", font=("Helvetica", 40), fg="orange")
+        self.etiquette_resultat = Label(self, text=f"{score} points", font=("Arial", 40), fg="black")
         self.etiquette_resultat.pack(pady=10) #pady signifie "padding y" pour ajouter de l'espace vertical entre les éléments ;)
         self.etiquette_resultat.place(x=10, y=10)
 
         # Bouton pour retourner à l'accueil
-        self.bouton_accueil = Button(self, text="Retourner à l'accueil",font=("Helvetica", 14),
+        self.bouton_accueil = Button(self, text="Retourner à l'accueil",font=("Arial", 14),
                                      command=lambda: [self.reset_page(), controleur.afficher_page(PageAccueil)])
         self.bouton_accueil.pack(pady=10)
         self.bouton_accueil.place(x=10, y=100)
 
          # Bouton pour relancer les dés
-        self.bouton_relancer = Button(self, text="Relancer les dés", font=("Helvetica", 14),
+        self.bouton_relancer = Button(self, text="Relancer les dés", font=("Arial", 14),
                                       command=self.relancer_des)
         self.bouton_relancer.pack(pady=10)
         self.bouton_relancer.place(x=1920 / 2 - 100, y=1080 / 2 + 150)
@@ -129,11 +130,11 @@ class InterfaceGraphique(Frame):
         """Affiche les dés avec leurs valeurs actuelles."""
         self.des_ids = []
         self.boutons_garder = []  # Liste des boutons "Garder"
-        x, y = 1920 / 2 - 150, 1080 / 2 - 50  # Position initiale des dés (remontés de 150 pixels)
+        x, y = 1920 / 2 - 150, 1080 / 2 - 50  # Position initiale des dés 
         for i, valeur in enumerate(self.valeurs_des):
             couleur = "green" if self.des_gardes[i] else "white"
-            des_id = self.canvas.create_rectangle(x, y, x + 50, y + 50, fill=couleur)
-            self.canvas.create_text(x + 25, y + 25, text=str(valeur), font=("Helvetica", 18))
+            des_id = self.canvas.create_rectangle(x, y, x + 50, y + 50, fill=couleur) #ICI UTILISER LES IMAGES DE DES A ITERER AVEC LE NOM DE FICHIER 
+            self.canvas.create_text(x + 25, y + 25, text=str(valeur), font=("Arial", 18))
             self.des_ids.append(des_id)
 
             # Ajouter un bouton "Garder" sous chaque dé
@@ -163,7 +164,7 @@ class InterfaceGraphique(Frame):
         combinaison = "-".join(map(str, self.valeurs_des))
         score = sum(self.valeurs_des)  # Exemple de calcul de score
         self.canvas.create_text(1920 / 2, 1080 / 2 + 200, text=f"Combinaison : {combinaison}\nScore : {score}",
-                                 font=("Helvetica", 18), fill="blue")
+                                 font=("Arial", 18), fill="blue")
 
     def reset_page(self):
         """Réinitialise la page de lancer de dés."""
@@ -193,13 +194,16 @@ class Shop(Frame):
         arriere_plan = PhotoImage(file='illustration/shop.png', master=racine)
         label_arriere_plan = Label(self, image=arriere_plan)
         label_arriere_plan.image = arriere_plan  # Garder une référence pour éviter que l'image ne soit supprimée
-        label_arriere_plan.place(relx=0.5, rely=0.5, anchor='center')
+        label_arriere_plan.place(relx=0.5, rely=0.5, anchor='center') 
 
         # Bouton pour retourner à l'accueil
-        self.bouton_accueil = Button(self, text="Retourner à l'accueil",font=("Helvetica", 14), command=lambda: controleur.afficher_page(PageAccueil))
+        self.bouton_accueil = Button(self, text="Retourner à l'accueil",font=("Arial", 14), command=lambda: controleur.afficher_page(PageAccueil))
         self.bouton_accueil.pack(pady=10)
-        self.bouton_accueil.place(x=170, y=880)
+        self.bouton_accueil.place(x=165, y=880)
 
+
+        
+'''AQUARIUM : Inventaire des poissons obtenus'''
 class Aquarium(Frame):
     def __init__(self, parent, controleur):
         Frame.__init__(self, parent)
@@ -210,7 +214,7 @@ class Aquarium(Frame):
         label_arriere_plan.place(relx=0.5, rely=0.5, anchor='center')
 
         # Bouton pour retourner à l'accueil
-        self.bouton_accueil = Button(self, text="Retourner à l'accueil",font=("Helvetica", 14), command=lambda: controleur.afficher_page(PageAccueil))
+        self.bouton_accueil = Button(self, text="Retourner à l'accueil",font=("Arial", 14), command=lambda: controleur.afficher_page(PageAccueil))
         self.bouton_accueil.pack(pady=10)
         self.bouton_accueil.place(x=165, y=800)
 
