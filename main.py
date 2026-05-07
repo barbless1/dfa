4810299dd355e7cb90602808f41be8d318b0a601
Eliott <Eliott@athys.net>
2026-05-07 09:39:31 +0200

    image moove

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ class InterfaceGraphique(Frame):
         self.bouton_lancer.pack(pady=5)
         
         #pour appeler la methode d'une autre classe, on doit d'abord créer une instance de cette classe, ici "jeu" est une instance de la classe "fonctions_du_jeu"
-        self.jeu = fonctions_du_jeu() 
+        self.jeu = fonctions_du_jeu(self) 
         self.bouton_lancer.config(command=self.jeu.lancer_des)
         self.bouton_lancer.place(x=1920/2-100, y=900)
         score = 0 
@@ -90,9 +90,11 @@ class InterfaceGraphique(Frame):
 
         #mouvement main
         self.img_bras = PhotoImage(file='illustration/main_fermé.png')
+        self.img_bras_ouverte = PhotoImage(file='illustration/main_ouverte.png')
         self.bras_id = self.canvas.create_image(200, 200, image=self.img_bras)
         self.canvas.bind("<Button-1>", self.start_drag)
         self.canvas.bind("<B1-Motion>", self.drag)
+        self.canvas.bind("<ButtonRelease-1>", self.release_drag)
 
     def start_drag(self, event):
         self.last_x, self.last_y = event.x, event.y
@@ -102,6 +104,10 @@ class InterfaceGraphique(Frame):
         dy = event.y - self.last_y
         self.canvas.move(self.bras_id, dx, dy)
         self.last_x, self.last_y = event.x, event.y
+
+    def release_drag(self, event):
+        # Lancer les dés (qui gère aussi le changement d'image)
+        self.jeu.lancer_des()
 
       
 '''BOUTIQUE DU JEU : page du shop'''
@@ -138,7 +144,8 @@ class Aquarium(Frame):
 '''PARTIE 3 : logique du jeu'''
 #valeur initial dès
 class fonctions_du_jeu:
-    def __init__(self):
+    def __init__(self, interface=None):
+        self.interface = interface
         self.de1 = randint(1, 6)
         self.de2 = randint(1, 6)
         self.de3 = randint(1, 6)
@@ -153,7 +160,16 @@ class fonctions_du_jeu:
     
     def lancer_des(self):
         # Logique pour lancer les dés et afficher le résultat
-        pass
+        self.de1 = randint(1, 6)
+        self.de2 = randint(1, 6)
+        self.de3 = randint(1, 6)
+        self.de4 = randint(1, 6)
+        self.de5 = randint(1, 6)
+        
+        # Mettre à jour l'interface si elle est disponible
+        if self.interface:
+            self.interface.canvas.itemconfig(self.interface.bras_id, image=self.interface.img_bras_ouverte)
+            self.interface.etiquette_resultat.config(text=f"score : {sum(self.liste_valeurs_dès())}")
 
     def relancer_des(self, des_a_relancer):
         """
